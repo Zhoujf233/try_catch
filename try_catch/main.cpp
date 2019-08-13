90e8c9cf7217ef68e1537a61b74e45a745c325bf
zhoujf <zhoujf1636@163.com>
2019-08-13 16:39:42 +0800

    try_catch

--- try_catch/main.cpp
+++ try_catch/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+    double triangle(double,double,double);
+    double a,b,c;
+    cout<<"please input three number:";
+    cin>>a>>b>>c;
+    try
+    {
+        while(a>0 && b>0 && c>0)
+        {
+            cout<<triangle(a,b,c)<<endl;
+            cin>>a>>b>>c;
+        }
+    }
+    catch(double)
+    {
+        cout<<"a = "<<a<<",b = "<<b<<",c = "<<c<<",that is not a triangle!"<<endl;
+        cout<<"end"<<endl;
+    }
+}
+
+double triangle(double a,double b,double c)
+{
+    double s = (a+b+c)/2;
+    if(a+b<=c||b+c<=a||a+c<=b)throw a;
+    return sqrt(s*(s - a)*(s - b)*(s - c));
 }
